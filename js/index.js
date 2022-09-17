89c2bde52eba39c4b78c1191fa15c0820bf41d38
1-EXON <exon.indiv@gmail.com>
2022-09-17 16:40:06 +0900

    Update: Design

--- js/index.js
+++ js/index.js
@@ -1,49 +1,10 @@
 class App {
-    latitude = 0.0
-    longitude = 0.0
-    locationOptions = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    }
 
     constructor() {
-        const registerHomeBtn = document.getElementById('registerHomeBtn')
 
         if (this.isDarkMode()) this.changeTheme(true)
 
-        if (!navigator.geolocation) return
-        navigator.geolocation.getCurrentPosition(async (position) => {
-            const container = document.getElementById('map')
-            const options = {
-                center: new kakao.maps.LatLng(position.coords.latitude, position.coords.longitude),
-                level: 3
-            }
-            const map = new kakao.maps.Map(container, options)
-            this.latitude = position.coords.latitude
-            this.longitude = position.coords.longitude
-            const markerPosition  = new kakao.maps.LatLng(this.latitude, this.longitude)
-            const marker = new kakao.maps.Marker({ position: markerPosition })
-            console.log(marker)
-            marker.setMap(map)
-            map.setCenter(markerPosition)
-
-            console.log(this.latitude, this.longitude)
-        }, this.error, this.locationOptions)
-
-        registerHomeBtn.onclick = async () => {
-            if (!('geolocation' in navigator)) {
-                alert('현재 위치 정보를 가져올 수 없습니다.')
-                return
-            }
-            localStorage.setItem('latitude', this.latitude)
-            localStorage.setItem('longitude', this.longitude)
-            alert('집 등록이 완료되었습니다.')
-        }
-    }
-
-    error(err) {
-        console.log(err)
+        
     }
 
     /**
